fc5d53895dc5a978205e22ea513c27a5db5e77d7
Hans Tananda <hanstananda@gmail.com>
2021-03-03 16:31:12 +0800

    added aggregator dockerfile and docker-compose

--- Aggregator/config/flask_config.py
+++ Aggregator/config/flask_config.py
@@ -3,6 +3,7 @@ All flask configurations shall be put here
 """
 
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -20,3 +21,8 @@ class DefaultConfig:
 
 class DockerConfig(DefaultConfig):
     SERVER_IP = "http://he-ew-demo-server:7000"
+
+
+class ProdConfig(DefaultConfig):
+    load_dotenv()
+    SERVER_IP = os.getenv("SERVER_IP")
--- Aggregator/run_server.py
+++ Aggregator/run_server.py
@@ -6,19 +6,23 @@ from gevent.pywsgi import WSGIServer
 from gevent import monkey
 
 from app import create_app
-from config.flask_config import AGGREGATOR_IP, DockerConfig, DefaultConfig
+from config.flask_config import *
 
 parser = argparse.ArgumentParser(description='Run the aggregator service')
 parser.add_argument('--server_ip', dest='server_ip', action='store',
                     help='specify server ip to be used', default=None)
 parser.add_argument('--is_docker', default=False, action='store_true',
                     help='specify whether docker IPs should be used')
+parser.add_argument('--is_prod', default=False, action='store_true',
+                    help='specify whether production environment is used, implying the use of .env')
 
 results = parser.parse_args()
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     if results.is_docker:
         config = DockerConfig
+    elif results.is_prod:
+        config = ProdConfig
     else:
         config = DefaultConfig
     app = create_app(config)
